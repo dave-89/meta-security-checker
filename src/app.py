c260c094cb8ac229d5c64db6beac1409e859bc1d
Dave <dave-89@users.noreply.github.com>
2018-12-22 23:42:58 +0000

    Feature/first workin (#3)
    * fixing return
    
    * with api key
    
    * adding JS
    
    * test

--- src/app.py
+++ src/app.py
@@ -1,11 +1,29 @@
 import os
-from flask import Flask, make_response, request, send_from_directory, current_app
+import json
+from tag_checker import get_http_equiv_tags
+from flask import Flask, abort, make_response, request, send_from_directory, current_app
 
 app = Flask(__name__)
 
-@app.route("/")
+api_key = os.environ.get("X-API-KEY", "x2I0l6N4Az8B")
+
+@app.route("/", methods = ['GET'])
 def landing_page():
     return send_from_directory('static', 'index.html')
+
+@app.route("/<path:path>", methods = ['GET'])
+def static_server(path):
+    return send_from_directory('static', path)
+
+@app.route("/check", methods = ['POST'])
+def check():
+    headers = request.headers
+    if headers['X-API-KEY'] != api_key:
+        abort(401)
+    data = json.loads(request.data)
+    return json.dumps({
+        "tags": get_http_equiv_tags(data['url'])
+    })
 
 
 if __name__ == '__main__':
--- src/tag_checker.py
+++ src/tag_checker.py
@@ -1,1 +1,15 @@
 import requests
+import re
+
+def get_http_equiv_tags(url):
+    resp = requests.get("https://drdave89.co.uk/")
+    _meta_tags = re.findall(r'<meta http-equiv=(.*) content=(.*)>', resp.text)
+    meta_tags = []
+    for tag in _meta_tags:
+        meta_tags.append(
+            [
+                tag[0].replace('"', ''),
+                tag[1].replace('"', '')
+                ]
+            )
+    return meta_tags
