5fe5ef3b00a046e1b907545aa1cdd001f470e49d
Dave <dave-89@users.noreply.github.com>
2018-12-22 19:34:33 +0000

    fixing return (#2)

--- src/app.py
+++ src/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def landing_page():
-    return return send_from_directory('static', 'index.html')
+    return send_from_directory('static', 'index.html')
 
 
 if __name__ == '__main__':
